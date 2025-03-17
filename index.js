3099adedb1600f5ce9c824ada17f7d7f3b8163e4
Joram Wayne <joramwmuya@gmail.com>
2025-03-17 11:45:07 +0300

    Update html and css file

---
+++ index.js
@@ -0,0 +1,19 @@
+const ramens = [
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+ ];
+//  function displayRamens(){
+//     const ramenMenu = document.getElementById
+// }
+
+// const btnone = document.getElementById('btn1');
+// const btntwo = document.getElementById('btn2');
+// const btnthree = document.getElementById('btn3');
+// const btnfour = document.getElementById('btn4');
+// const btnfive = document.getElementById('btn5');
+
+//  const submitt = document.getElementById('submit-btn');
+//  submitt.addEventListener('click', () => {
+
+//  })
